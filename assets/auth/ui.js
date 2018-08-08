6c110d560af38ea044c531942d361be3d73f792b
j0nathan <jonathan.j.dejesus85@gmail.com>
2018-08-08 08:41:19 -0400

    Added Auth folder with api, events and ui files for authentication

---
+++ assets/auth/ui.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const store = require('../scripts/store')
+// const userEvents = require('../user/events')
+
+const signUpSuccess = function () {
+  $('#auth-modal').remove()
+  // $('.content').append(authTemplate)
+  // Need more work!
+}
+
+const signInSuccess = function (data) {
+  $('#auth-modal').hide()
+  store.user = data.user
+  $('.navbar').toggle('display')
+  $('#sign-in-form')[0].reset()
+  $('#sign-up-form')[0].reset()
+}
+
+const changePasswordSuccess = function () {
+}
+
+const signOutSuccess = function () {
+  store.user = {}
+  store.devices = []
+  store.repairs = []
+  // userEvents.clearContent()
+
+  // Close toggle menu (Mobile View)
+  $('.navbar-toggle').hasClass('collapsed') ? 
+    '' : $('.navbar-toggle').click()
+
+  $('.wrapper').css('display', 'none')
+  $('.main').css('display', 'block')
+  $('.navbar').toggle('display')
+  $('#auth-modal').show()
+}
+
+const failure = function (error) {
+  console.log('failure ran. Data is:', error)
+}
+
+module.exports = {
+  signUpSuccess,
+  signInSuccess,
+  changePasswordSuccess,
+  signOutSuccess,
+  failure
+}
