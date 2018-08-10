e20de4bbe092042ac67baa8b9d3c59e328854457
Ashley <ashley.ratigan@gmail.com>
2018-08-10 10:14:37 -0400

    Updated styling for seeing past orders

--- assets/scripts/orders/ui.js
+++ assets/scripts/orders/ui.js
@@ -8,6 +8,9 @@ const getOrdersSuccess = (data) => {
   const showOrdersHtml = showOrdersTemplate({ orders: data.orders })
   const showOrdersItemsHtml = showOrdersItemsTemplate({ items: data.orders.items })
   $('.content').html(showOrdersHtml)
+  setTimeout(function () {
+    $('#userInfoModal').modal('hide')
+  }, 2000)
 }
 
 const failure = (error) => {
