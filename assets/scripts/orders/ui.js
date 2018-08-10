725b8d9e0e7da001711c8e7f23d9b806dd50af67
Ashley <ashley.ratigan@gmail.com>
2018-08-10 11:04:32 -0400

    Trying to view current orders

--- assets/scripts/orders/ui.js
+++ assets/scripts/orders/ui.js
@@ -1,17 +1,49 @@
 'use strict'
 
 const showOrdersTemplate = require('../templates/order.handlebars')
-const showOrdersItemsTemplate = require('../templates/order-items.handlebars')
+const store = require('../store')
+// const showOrdersItemsTemplate = require('../templates/order-items.handlebars')
 
 const getOrdersSuccess = (data) => {
   console.log(data)
-  const showOrdersHtml = showOrdersTemplate({ orders: data.orders })
-  const showOrdersItemsHtml = showOrdersItemsTemplate({ items: data.orders.items })
-  $('.content').html(showOrdersHtml)
+  for (let i = 0; i < data.orders.length; i++) {
+    if (data.orders[i].submitted === true) {
+      const orders = data.orders
+      // console.log('1')
+      // console.log(orders)
+      // console.log('1')
+      const showOrdersHtml = showOrdersTemplate({ orders: orders })
+      // const showOrdersItemsHtml = showOrdersItemsTemplate({ items: data.orders.items })
+      $('.content').html(showOrdersHtml)
+    } else if (data.orders[i].submitted === false) {
+      store.user.cart = data.orders
+    }
+  }
+  console.log(store)
+  
   setTimeout(function () {
     $('#userInfoModal').modal('hide')
   }, 2000)
 }
+
+// const getCurrentOrderSuccess = (data) => {
+//   console.log(data)
+//   for (let i = 0; i < data.orders.length; i++) {
+//     if (data.orders[i].submitted === true) {
+//       const orders = data.orders
+//       // console.log('1')
+//       // console.log(orders)
+//       // console.log('1')
+//       const showOrdersHtml = showOrdersTemplate({ orders: orders })
+//       // const showOrdersItemsHtml = showOrdersItemsTemplate({ items: data.orders.items })
+//       $('.content').html(showOrdersHtml)
+//     }
+//   }
+
+//   setTimeout(function () {
+//     $('#userInfoModal').modal('hide')
+//   }, 2000)
+// }
 
 const failure = (error) => {
   console.error(error)
@@ -19,5 +51,6 @@ const failure = (error) => {
 
 module.exports = {
   getOrdersSuccess,
+  // getCurrentOrderSuccess,
   failure
 }
