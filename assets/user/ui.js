27b3cc5188da52beacef9bfcba29da867e9de52f
j0nathan <jonathan.j.dejesus85@gmail.com>
2018-08-11 21:38:04 -0400

    Fixed addToCart Logic
    If the user has no open orders a new order is generated.
    Otherwise the open order is recreated (persistent shopping cart)
    
    This logic was worked on by all members of my group.

--- assets/user/ui.js
+++ assets/user/ui.js
@@ -4,13 +4,18 @@ const store = require('../scripts/store')
 
 
 const createOrderSuccess = (data) => {
-    // console.log("Data is: ", data)
-    // console.log("Order ID is: ", data.order._id)
     store.userData.order_id = data.order._id
-    console.log(store.userData.order_id)
+    store.userData.cart = data.order
+
+}
+
+const updateOrderSucces = (data) => {
+    // console.log(data)
+    // store.userData.cart = data.order
 }
 
 
 module.exports = {
-    createOrderSuccess
+    createOrderSuccess,
+    updateOrderSucces
 }
