c6435da909378906e63c0a21a06a15a2b3b96cdc
mkzarra <mkzarra@gmail.com>
2018-08-12 16:53:05 -0400

    added a route to action attr. for stripe form

--- assets/user/events.js
+++ assets/user/events.js
@@ -57,7 +57,7 @@ const userHandlers = () => {
     $('#stripe-input').val('Bearer ' + store.user.token)
     $('.stripe-button').attr('data-amount', store.userData.cart.total)
     $('.stripe-button').attr('data-description', `Nozama ${store.userData.cart._id}`)
-    $('#stripe-form').attr('action', config.apiUrls)
+    $('#stripe-form').attr('action', config.apiUrls + '/charge')
 }
 
 module.exports = {
